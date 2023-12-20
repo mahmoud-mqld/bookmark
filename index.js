fa6a85425d0da3d1d04a4d7b06d3846f3b91b4b9
mahmoud-mqld <mqldm@yahoo.com>
2023-12-21 00:22:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -63,13 +63,8 @@ function display(bookmarks) {
 <td></td>
 <td>  <a class="btns btn-2" target="_blank" href="${
       bookmarks[index].siteURL
-    }">visit <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
-    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
-    <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
-  </svg></a> </td>
-<td> <button class="btns btn-3" onclick="remove(${index})" >remove <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-<path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
-</svg></button> </td>
+    }">visit <i class="fa-solid fa-eye fs-6"></i></a> </td>
+<td> <button class="btns btn-3" onclick="remove(${index})" >remove <i class="fa-solid fa-trash fs-6"></i></button> </td>
  </tr>
 `;
   }
